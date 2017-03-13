52ab79944b0cabff5dd4837989bd5c018e3dccca
wutao1530663 <569552388@qq.com>
2017-03-13 20:35:33 +0800

    实现变量存储和环境变量功能2

--- my_shell.h
+++ my_shell.h
@@ -1,13 +1,43 @@
 #define is_delimiter(c) (((c) == ';') ||((c) == '\n'))
 #define ARGLEN 20
 #define MAXARGS 20
+#define MAXVARS 300
 #include<unistd.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<ctype.h>
+#include<sys/wait.h>
+#include<signal.h>
+//splitline.c
 char *next_cmd(char*,FILE*);
 char **splitline(char *);
+
+//alloc and free memory
 void freelist(char**);
 void *emalloc(size_t);
 void *erealloc(void*,size_t);
-int execute(char**);
 
+//process cmd
+int execute(char**);
+int process(char**);
+
+//controlflow.c
+int ok_to_execute();
+int do_control_command(char**);
+int is_control_command(char*);
+
+//builtin.c
+
+int is_built_in_command(char*);
+int do_built_in_command(char**);
+
+//var.c
+int var_parse_and_store(char*);
+int var_store(char*,char*);
+int var_export();
+void var_print_list();
+int var_init_from_environ(char**);
+char** var_to_environ();
+
+//util.c
+int is_legal_name(char*);
